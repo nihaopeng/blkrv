ffa3ab51e57ce3fe2b75318a9b0a953a48e192ca
helloyutao <14049665+helloyutao@user.noreply.gitee.com>
2024-09-03 19:19:29 +0800

    2024.9.3,make a little bench for rib

--- rtl/vsrc/core/cpu.v
+++ rtl/vsrc/core/cpu.v
@@ -1,22 +1,26 @@
 
 module cpu(
     input clk_i,
-    output[31:0] pc_o,
-    output[31:0] data_o
+    output reg[31:0] addr_o,
+    input[31:0] data_i,
+    output reg[31:0] data_o,
+
+    output read_req_o,
+    output we_o,
+    input read_enable_i
 );
-wire cur_inst;
-wire[31:0] addr_t;
+wire[31:0] pc_val;
+wire[31:0] cur_inst;
+assign read_req_o=1'b1;
+assign we_o=1'b0;
 ifu ifu(
-    .clk(clk),
-    .inst_i(data),
+    .clk(clk_i),
+    .inst_i(data_i),
     .pc_val_o(pc_val),
     .instruction_o(cur_inst)
 );
 mmu mmu(
     .addr_i(pc_val),
-    .addr_t_o(addr_t)
-);
-rib rib(
-    
+    .addr_t_o(addr_o)
 );
 endmodule
--- rtl/vsrc/top.v
+++ rtl/vsrc/top.v
@@ -1,11 +1,58 @@
 module top(
     input clk,
-    output[31:0] pc_val,
-    output[31:0] data
+    output[31:0] s0_addr,
+    output[31:0] s0_write_data,
+    input[31:0] s0_read_data,
+    input s0_read_enable,
+    output s0_we,
 );
+wire[31:0] addr;
+wire[31:0] read_data;
+wire[31:0] write_data;
+wire m0_read_req;
+wire m0_we;
+wire re;
 cpu cpu(
-    .clk(clk),
-    .pc_val(pc_val),
-    .data(data)
+    .clk_i(clk),
+    .addr_o(addr),
+    .data_i(read_data),
+    .data_o(write_data),
+    .read_req_o(m0_read_req),
+    .we_o(m0_we),
+    .read_enable_i(re)
+);
+rib rib(
+    .clk(clk_i),  
+    .rst(1'b0),   
+    //master0
+    .m0_addr_i(addr),
+    .m0_write_data_i(write_data),
+    .m0_read_data_o(read_data),
+    .m0_req_i(m0_read_req),
+    .m0_we_i(m0_we),
+
+    //master1
+    // input[31:0] m1_addr_i,
+    // input[31:0] m1_write_data_i,
+    // output reg[31:0] m1_read_data_o,
+    // input m1_req_i,
+    // input m1_we_i,
+
+    //slave0
+    .s0_addr_o(s0_addr),
+    .s0_write_data_o(s0_write_data),
+    .s0_read_data_i(s0_read_data),
+    .s0_read_enable_i(s0_read_enable),
+    .s0_we_o(s0_we),
+
+    //slave1
+    // output reg[31:0] s1_addr_o,
+    // output reg[31:0] s1_write_data_o,
+    // input[31:0] s1_read_data_i,
+    // input s1_read_enable_i,
+    // output reg s1_we_o,
+    
+    .hold_flag_o(),
+    .read_enable_o(re)
 );
 endmodule
