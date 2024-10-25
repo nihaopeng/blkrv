b595cbd9ef51179adfc2153ad0e7a278941d739f
nameless <15010627+c-nameless@user.noreply.gitee.com>
2024-10-25 23:16:09 +0800

    optimize flash init

--- rtl/csrc/src/devices.cpp
+++ rtl/csrc/src/devices.cpp
@@ -3,7 +3,7 @@
 devices::devices(){
     // printf("1");
     std::cout<<"$init bios"<<std::endl;
-    this->my_bios=new bios("./devices/bios",1<<21-1);
+    this->my_bios=new bios("./devices/bios",1<<21);
     std::cout<<"$init keyboard"<<std::endl;
     this->my_keyboard=new keyboard(4);
     std::cout<<"$init ram"<<std::endl;
@@ -11,9 +11,9 @@ devices::devices(){
     std::cout<<"$init flash"<<std::endl;
     this->my_flash=new flash("./devices/flash",1<<28);
     std::cout<<"$init screen"<<std::endl;
-    this->my_screen=new screen(1<<20-1);
+    this->my_screen=new screen(1<<20);
     std::cout<<"$init net_card"<<std::endl;
-    this->my_net_card=new net_card(1<<20-1);
+    this->my_net_card=new net_card(1<<20);
     std::cout<<"$init pmc"<<std::endl;
     this->my_pmc=new pmc(0);
     system("make loadmem");
--- rtl/csrc/src/nvmem.cpp
+++ rtl/csrc/src/nvmem.cpp
@@ -4,13 +4,17 @@ nvmem::nvmem(std::string mem_file_path,uint32_t size){
     this->mem_file_path=mem_file_path;
     if(access((mem_file_path).c_str(),F_OK)!=0){//file not exist
         this->fp.open(mem_file_path,std::ios::binary|std::ios::out);
-        uint32_t d=0;
-        for(uint64_t i=0;i<size;i+=4){
-            printf("\r(size:%dByte)process:%f/100",size,(float(i+4)/size)*100);
-            this->fp.seekp(i);
-            this->fp.write(reinterpret_cast<const char*>(&d),sizeof(d));
+        // uint32_t d=0;
+        // for(uint64_t i=0;i<size;i+=4){
+        //     printf("\r(size:%dByte)process:%f/100",size,(float(i+4)/size)*100);
+        //     this->fp.seekp(i);
+        //     this->fp.write(reinterpret_cast<const char*>(&d),sizeof(d));
+        // }
+        char* write_data=(char*)malloc(sizeof(char)*256*1024*1024);
+        if (write_data != NULL) {
+            memset(write_data, 0, sizeof(char)*256*1024*1024); // 初始化为0
         }
-        std::cout<<std::endl;
+        this->fp.write(write_data,256*1024*1024);
         this->fp.close();
     }else{
         this->fp.open(mem_file_path,std::ios::binary|std::ios::out|std::ios::in);//同时使用in和out是为了修改某一部分内容而不清空文件
